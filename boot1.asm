4e7fbf765868b8446daa5bfd9185b7a4ccb9ff5f
Valgueiro <mvt@cin.ufpe.br>
2017-04-13 11:21:03 -0300

    Commenting things

--- boot1.asm
+++ boot1.asm
@@ -3,44 +3,38 @@ org 0x7C00
 jmp 0x0000:start; garantir cs == 0x0000
 
 start:
-    ; nunca se esqueca de zerar o ds,
-	; pois a partir dele que o processador busca os 
-	; dados utilizados no programa.
 	xor ax, ax
 	mov ds, ax
 
-	;Início do seu código
-
+;Resetando o disco floppy, forçando também a setar todas as trilhas para 0
 reset:
-	;Resetando o disco floppy
 	mov ah,0		
 	mov dl,0		
 	int 13h			
 	jc reset		;em caso de erro, tenta de novo, 
-					;ja que a int seta o carry para 1 caso algo de errado aconteça
 
-;Carregar da memoria o boot2
+;Carrega na memoria o boot2
 load_Boot2:
-	;Setando a posição do disco onde boot2.asm foi armazenado(ES:BX = [0x500:0x0])
+;Setando a posição do disco onde boot2.asm foi armazenado(ES:BX = [0x500:0x0])
 	mov ax,0x50		;0x50<<1 + 0 = 0x500
 	mov es,ax
 	xor bx,bx		;Zerando o offset
 
-	;Setando a posição da Ram onde o boot2 será lido
+;Setando a posição da Ram onde o boot2 será lido
 	mov ah, 0x02	;comando de ler setor do disco
 	mov al,1		;quantidade de blocos ocupados por boot2
+	mov dl,0		;drive floppy
+
+;Usaremos as seguintes posições na memoria:
 	mov ch,0		;trilha 0
 	mov cl,2		;setor 2
 	mov dh,0		;cabeca 0
-	mov dl,0		;drive floppy
 	int 13h
-	jc load_Boot2		;deu erro, tenta de novo
+	jc load_Boot2	;em caso de erro, tenta de novo
 
 break:	
-	jmp 0x500
-	;Fim do seu código. 
-	
+	jmp 0x500 		;Pula para a posição carregada
 
+end: 
 times 510-($-$$) db 0		; preenche o resto do setor com zeros 
-dw 0xaa55					; coloca a assinatura de boot no final
-							; do setor (x86 : little endian)
+dw 0xaa55					; coloca a assinatura de boot no final do setor
