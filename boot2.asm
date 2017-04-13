ee60e61c4a361ab639b45ace3d0d1889e841b590
Valgueiro <mvt@cin.ufpe.br>
2017-04-13 11:11:42 -0300

    Bootloader pronto da forma como manda o teacher

--- boot2.asm
+++ boot2.asm
@@ -1,9 +1,7 @@
 org 0x500; 
 jmp 0x000: start
 
-%macro printString 1
-     mov   si, %1
-      
+printString:    
 	print:
 		lodsb
 		cmp al, 0
@@ -14,12 +12,26 @@ jmp 0x000: start
 
 		jmp print
 	exit:
-%endmacro
-
-strInicial db 'Ola meus beberes', 0
+ret
 
 start:
-	printString strInicial
+	mov si, str1
+	call printString
+
+	mov si, str2
+	call printString
+	
+	mov si, str3
+	call printString
+	
+	mov si, str4
+	call printString
+
+
+str1 db 'Loading structures for the kernel...', 10, 13, 0
+str2 db 'Setting up protected mode...', 10, 13, 0
+str3 db 'Loading kernel in memory...', 10 , 13, 0
+str4 db 'Running kernel...', 10, 13, 0
 
 times 510-($-$$) db 0		; preenche o resto do setor com zeros 
 dw 0xaa55					; coloca a assinatura de boot no final
